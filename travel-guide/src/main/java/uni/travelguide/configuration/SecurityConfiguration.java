695b1add94a091e6a7c5a90e3cb7895b876c0941
Stoyanov22 <iliyan.stoyanov.22@gmail.com>
2019-12-04 17:39:07 +0200

    Updated roles query. Enabled h2-console.

--- travel-guide/src/main/java/uni/travelguide/configuration/SecurityConfiguration.java
+++ travel-guide/src/main/java/uni/travelguide/configuration/SecurityConfiguration.java
@@ -25,19 +25,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     private DataSource dataSource;
 
     private final String USERS_QUERY = "select email, password, active from user where email=?";
-    private final String ROLES_QUERY = "select u.email, r.role from user u inner join user_role ur on (u.id = ur.user_id) inner join role r on (ur.role_id=r.role_id) where u.email=?";
 
     @Override
     protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
         authenticationManagerBuilder.jdbcAuthentication()
                 .usersByUsernameQuery(USERS_QUERY)
-                .authoritiesByUsernameQuery(ROLES_QUERY)
+                .authoritiesByUsernameQuery(USERS_QUERY)
                 .dataSource(dataSource)
                 .passwordEncoder(bCryptPasswordEncoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
+        http.headers().frameOptions().sameOrigin();
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/login").permitAll()
